40e2866cfd4e2e7734b5b432c9b1aa227227ea48
Alexis Bonnet <a.bonnet@codeur.online>
2022-07-08 16:45:18 +0200

    CRUD-catégories

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -32,8 +32,6 @@ class DashboardController extends AbstractController
             'controller_name' => 'DashboardController',
         ]);
     }
-    
-
     /**
      * @Route("/tableau_de_bord/achats", name="achats")
      */
@@ -223,7 +221,7 @@ class DashboardController extends AbstractController
             $manager->persist($achat);
             $manager->flush();
 
-            // return $this->redirectToRoute('visualisation_achat', ['id' => $achat->getId()]);
+            return $this->redirectToRoute('visualisation_achat', ['id' => $achat->getId()]);
         }
 
         return $this->render('tableau_de_bord/modification_achat.html.twig', [
@@ -249,12 +247,16 @@ class DashboardController extends AbstractController
         $achat = $achats->find($id);
 
         @unlink($this->getparameter('direction_tickets') . '/' . $achat->getTicketAchat());
+        @unlink($this->getparameter('direction_manuels') . '/' . $achat->getManuelUtilisation());
 
         $manager->remove($achat);
         $manager->flush();
 
         return $this->redirectToRoute('achats');
     }
+
+
+
     /**
      * @Route("/tableau_de_bord/statistiques", name="statistiques")
      */
@@ -324,5 +326,226 @@ class DashboardController extends AbstractController
                 'fin' => $form['fin_periode']->getData()
             ]
         ]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * @Route("/tableau_de_bord/categories", name="categories")
+     */
+    public function categories(): Response
+    {
+        $repoAchats = $this->getDoctrine()->getRepository(Achats::class);
+        $repoCategories = $this->getDoctrine()->getRepository(Categories::class);
+
+        $achats = $repoAchats->findAll();
+        $categories = $repoCategories->findAll();
+
+        return $this->render('tableau_de_bord/categories.html.twig', [
+            'controller_name' => 'DashboardController',
+            'achats' => $achats,
+            'categories' => $categories
+        ]);
+    }
+    /**
+     * @Route("/tableau_de_bord/secteurs/ajout", name="ajout_secteur")
+     */
+    public function ajout_secteur(Request $request, EntityManagerInterface $manager, SluggerInterface $slugger): response
+    {
+        $secteur = new Categories();
+
+        $form = $this->createFormBuilder($secteur)
+                     ->add('name')
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $secteur->setSlug("texte");
+
+            $manager->persist($secteur);
+            $manager->flush();
+
+            return $this->redirectToRoute('categories');
+       }
+
+        return $this->render('tableau_de_bord/ajout_secteur.html.twig', [
+            'form_ajout_secteur' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/tableau_de_bord/categories/ajout", name="ajout_categorie")
+     */
+    public function ajout_categorie(Request $request, EntityManagerInterface $manager, SluggerInterface $slugger): response
+    {
+        $repoSecteurs = $this->getDoctrine()->getRepository(Categories::class);
+        $secteurs = $repoSecteurs->findAll();
+
+        $options_secteur = ['' => NULL];
+        foreach ($secteurs as $secteur) {
+            if ($secteur->getParent() == NULL) {
+                $options_secteur[$secteur->getName()] = $secteur;
+            }
+        }
+
+        $categorie = new Categories();
+
+        $form = $this->createFormBuilder($categorie)
+                     ->add('name')
+                     ->add('parent', ChoiceType::class, [
+                        'choices' => $options_secteur,
+                        'constraints' => [
+                            new Assert\NotNull([
+                                'message' => "Vous devez choisir un secteur."
+                            ])
+                        ]
+                     ])
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $categorie->setSlug("texte");
+
+            $manager->persist($categorie);
+            $manager->flush();
+
+            return $this->redirectToRoute('categories');
+       }
+
+        return $this->render('tableau_de_bord/ajout_categorie.html.twig', [
+            'form_ajout_categorie' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/tableau_de_bord/secteurs/{id}/modification", name="modification_secteur")
+     */
+    public function modification_secteur($id, Request $request, EntityManagerInterface $manager, SluggerInterface $slugger) :response
+    {
+        $secteurs = $this->getDoctrine()->getRepository(Categories::class);
+        $secteur = $secteurs->find($id);
+
+        $form = $this->createFormBuilder($secteur)
+                     ->add('name')
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $secteur->setSlug("texte");
+
+            $manager->persist($secteur);
+            $manager->flush();
+
+            return $this->redirectToRoute('categories');
+        }
+
+        return $this->render('tableau_de_bord/modification_secteur.html.twig', [
+            'form_modification_secteur' => $form->createView(),
+            'id' => $id
+        ]);
+    }
+    /**
+     * @Route("/tableau_de_bord/categories/{id}/modification", name="modification_categorie")
+     */
+    public function modification_categorie($id, Request $request, EntityManagerInterface $manager, SluggerInterface $slugger) :response
+    {
+        $repoSecteurs = $this->getDoctrine()->getRepository(Categories::class);
+        $secteurs = $repoSecteurs->findAll();
+
+        $options_secteur = ['' => NULL];
+        foreach ($secteurs as $secteur) {
+            if ($secteur->getParent() == NULL) {
+                $options_secteur[$secteur->getName()] = $secteur;
+            }
+        }
+
+        $categories = $this->getDoctrine()->getRepository(Categories::class);
+        $categorie = $categories->find($id);
+
+        $form = $this->createFormBuilder($categorie)
+                     ->add('name')
+                     ->add('parent', ChoiceType::class, [
+                        'choices' => $options_secteur
+                     ])
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $categorie->setSlug("texte");
+
+            $manager->persist($categorie);
+            $manager->flush();
+
+            return $this->redirectToRoute('categories');
+        }
+
+        return $this->render('tableau_de_bord/modification_categorie.html.twig', [
+            'form_modification_categorie' => $form->createView(),
+            'id' => $id
+        ]);
+    }
+    /**
+     * @Route("/tableau_de_bord/categories/{id}/suppression", name="suppression_categorie")
+     */
+    public function suppression_categorie($id): response
+    {
+        $repoAchats = $this->getDoctrine()->getRepository(Achats::class);
+        $achats = $repoAchats->findAll();
+
+        $repoCategories = $this->getDoctrine()->getRepository(Categories::class);
+        $categories = $repoCategories->findAll();
+        $categorie_a_supprimer = $repoCategories->find($id);
+
+        $type = NULL;
+        $suppression_impossible = 0;
+        if ($categorie_a_supprimer->getParent() == NULL) {
+            $type = 'secteur';
+            foreach ($categories as $categorie) {
+                if ($categorie->getParent() != NULL && $categorie->getParent()->getId() == $categorie_a_supprimer->getId()) {
+                    $suppression_impossible++;
+                }
+            }
+        } else {
+            $type = 'categorie';
+            foreach ($achats as $achat) {
+                if ($achat->getCategorie()->getId() == $categorie_a_supprimer->getId()) {
+                    $suppression_impossible++;
+                }
+            }
+        }
+
+        return $this->render('tableau_de_bord/suppression_categorie.html.twig', [
+            'id' => $id,
+            'suppression_impossible' => $suppression_impossible,
+            'type' => $type
+        ]);
+    }
+    /**
+     * @Route("/tableau_de_bord/categorie/{id})/suppression/valide", name="suppression_categorie_valide")
+     */
+    public function suppression_categorie_valide($id, EntityManagerInterface $manager): response
+    {
+        $categories = $this->getDoctrine()->getRepository(Categories::class);
+        $categorie = $categories->find($id);
+
+        $manager->remove($categorie);
+        $manager->flush();
+
+        return $this->redirectToRoute('categories');
     }
 }
